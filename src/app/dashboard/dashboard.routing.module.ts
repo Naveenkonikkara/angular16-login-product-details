106fcabdd14c02c00bc925b0d259e153e78dcde9
Naveenkonikkara <naveenkonikkara@gmail.com>
2023-09-12 01:36:20 +0530

    Canactivate Product gaurd

--- src/app/dashboard/dashboard.routing.module.ts
+++ src/app/dashboard/dashboard.routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, mapToCanDeactivate } from '@angular/router';
 import { DirtyCheckGuard } from '../helpers/dirty-check.guard';
+import { ProductGuard } from '../helpers/product.guard';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
-  { path: 'products', component: ProductsComponent },
-  { path: 'product/:id', component: ProductDetailsComponent, canDeactivate: mapToCanDeactivate([DirtyCheckGuard]), },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    canActivate: [ProductGuard],
+  },
+  {
+    path: 'product/:id',
+    component: ProductDetailsComponent,
+    canDeactivate: mapToCanDeactivate([DirtyCheckGuard]),
+  },
 ];
 
 @NgModule({
